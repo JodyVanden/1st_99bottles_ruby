4e6538c79ff86e505ae2cfa50abcc7fea9e12db4
Jody Vandenschrick <jody.vandenschrick@afterpay.com>
2021-07-18 21:03:27 +1000

    Fix tests

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe "99 bottles song" do
       99 bottles of beer on the wall, 99 bottles of beer.
       Take one down and pass it around, 98 bottles of beer on the wall.
     VERSE
-    assert_equal expected, ::Bottles.new.verse(99)
+    newVerse = Bottles.new.verse(99)
+    expect(newVerse).to eq(expected)
   end
 
   it "should test another verse" do
@@ -15,7 +16,8 @@ RSpec.describe "99 bottles song" do
       89 bottles of beer on the wall, 89 bottles of beer.
       Take one down and pass it around, 88 bottles of beer on the wall.
     VERSE
-    assert_equal expected, ::Bottles.new.verse(89)
+    newVerse = Bottles.new.verse(89)
+    expect(newVerse).to eq(expected)
   end
 
   it "should test the second verse" do
@@ -23,7 +25,8 @@ RSpec.describe "99 bottles song" do
       2 bottles of beer on the wall, 2 bottles of beer.
       Take one down and pass it around, 1 bottle of beer on the wall.
     VERSE
-    assert_equal expected, ::Bottles.new.verse(2)
+    newVerse = Bottles.new.verse(2)
+    expect(newVerse).to eq(expected)
   end
 
   it "should test the first verse" do
@@ -31,7 +34,8 @@ RSpec.describe "99 bottles song" do
       1 bottle of beer on the wall, 1 bottle of beer.
       Take it down and pass it around, no more bottles of beer on the wall.
     VERSE
-    assert_equal expected, ::Bottles.new.verse(1)
+    newVerse = Bottles.new.verse(1)
+    expect(newVerse).to eq(expected)
   end
 
   it "should test the zero verse" do
@@ -39,7 +43,8 @@ RSpec.describe "99 bottles song" do
       No more bottles of beer on the wall, no more bottles of beer.
       Go to the store and buy some more, 99 bottles of beer on the wall.
     VERSE
-    assert_equal expected, ::Bottles.new.verse(0)
+    newVerse = Bottles.new.verse(0)
+    expect(newVerse).to eq(expected)
   end
 
   it "should test a couple of verses" do
@@ -50,7 +55,8 @@ RSpec.describe "99 bottles song" do
       98 bottles of beer on the wall, 98 bottles of beer.
       Take one down and pass it around, 97 bottles of beer on the wall.
     VERSES
-    assert_equal expected, ::Bottles.new.verses(99, 98)
+    newVerse = Bottles.new.verses(99, 98)
+    expect(newVerse).to eq(expected)
   end
 
   it "should test a few of verses" do
@@ -64,7 +70,8 @@ RSpec.describe "99 bottles song" do
       No more bottles of beer on the wall, no more bottles of beer.
       Go to the store and buy some more, 99 bottles of beer on the wall.
     VERSES
-    assert_equal expected, ::Bottles.new.verses(2, 0)
+    newVerse = Bottles.new.verses(2, 0)
+    expect(newVerse).to eq(expected)
   end
 
   it "should test the whole song" do
@@ -369,6 +376,7 @@ RSpec.describe "99 bottles song" do
       No more bottles of beer on the wall, no more bottles of beer.
       Go to the store and buy some more, 99 bottles of beer on the wall.
     SONG
-    assert_equal expected, ::Bottles.new.song
+    newVerse = Bottles.new.song
+    expect(newVerse).to eq(expected)
   end
 end
